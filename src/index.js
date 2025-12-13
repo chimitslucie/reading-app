0cca374086b695104ebb0812f99ccba80e49f537
chimitslucie <chimitslucie@gmail.com>
2025-12-13 08:49:28 +0100

    create pages + create route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./Style/main.scss";
 import HomePage from "./Pages/homePage";
+import BookInProgress from "./Pages/bookInProgress";
+import BookRead from "./Pages/bookRead";
+import Library from "./Pages/library";
+import PileToRead from "./Pages/pileToRead";
+import Wishlist from "./Pages/wishlist";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,6 +16,11 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/Wishlist" element={<Wishlist />} />
+        <Route path="/Bibliotheque" element={<Library />} />
+        <Route path="/PileALire" element={<PileToRead />} />
+        <Route path="/LivresEnCours" element={<BookInProgress />} />
+        <Route path="/LivresLus" element={<BookRead />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
