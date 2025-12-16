5410ca3232447dd5354b53a28bb5156e63118772
chimitslucie <chimitslucie@gmail.com>
2025-12-16 16:52:18 +0100

    add nav component

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,25 +1,49 @@
 import logo from '../Assets/Images/logo.png';
-import { Link } from 'react-router';
+import { NavLink, Link } from "react-router";
+import { useState } from "react";
 
 function Nav() {
+
+    const [showNav, setShowNav] = useState(false);
+
+    const handleShowNav = () => {
+        setShowNav(!showNav)
+    }
+
     return (
         <div
-            className="nav"
+            className="header"
         >
-            <img src={logo} alt="livre" />
-            <div className="navBar">
+            <Link className='logoLink' to={'/'}>
+                <img src={logo} alt="livre" className='logo' />
+            </Link>
+            <div id="navToggle" className={`navToggle ${showNav ? "toCross" : ""}`} onClick={handleShowNav}>
+                <span className="navToggleBar navToggleBarTop"></span>
+                <span className="navToggleBar navToggleBarMiddle"></span>
+                <span className="navToggleBar navToggleBarBottom"></span>
+            </div>
+            <nav className={`nav ${showNav ? "navOpen" : ""}`}>
                 <ul className='navList'>
                     <li className="navItem">
-                        <Link className='navLink' to={'/'}>Home</Link>
+                        <NavLink className='navLink' to="/" end> Home </NavLink>
                     </li>
                     <li className="navItem">
-                        <Link className='navLink' to={'/'}>Wishlist</Link>
+                        <NavLink className='navLink' to="/Wishlist" end> Wishlist </NavLink>
                     </li>
                     <li className="navItem">
-                        <Link className='navLink' to={'/'}>Bibliothèque</Link>
+                        <NavLink className='navLink' to="/Bibliotheque" end> Bibliothèque </NavLink>
+                    </li>
+                    <li className="navItem">
+                        <NavLink className='navLink' to="/PileALire" end> Pile à Lire </NavLink>
+                    </li>
+                    <li className="navItem">
+                        <NavLink className='navLink' to="/LivresEnCours" end> Livre en Cours </NavLink>
+                    </li>
+                    <li className="navItem">
+                        <NavLink className='navLink' to="/LivresLus" end> Livres Lus </NavLink>
                     </li>
                 </ul>
-            </div>
+            </nav>
         </div>
     );
 }
